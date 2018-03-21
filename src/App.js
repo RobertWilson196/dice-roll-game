cf7a7b460c891f4ee9ae84a6a73d71fa3b3f35c2
Robert Wilson <youmufan@gmail.com>
2018-03-21 11:11:13 -0400

    added a diceRoll function that generated a random integer (1 - 6), which updates a dice value

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      diceOne: 0
+    }
+    this.diceRoll = this.diceRoll.bind(this);
+  }
+
+  diceRoll(){
+    let rollValue = (Math.floor(Math.random() * 6)) + 1;
+    this.setState({diceOne: rollValue});
+    return rollValue;
+  }
+
   render() {
     return (
       <div>
-        Hello
+        <h1>{this.state.diceOne}</h1>
+        <button onClick={() => this.diceRoll()}>Roll Dice</button>
       </div>
     );
   }
